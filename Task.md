11a203580acdf4d2ffdc0c71c1e96a892dd2f433
Singh, Atin <>
2021-04-03 15:13:16 -0400

    Added Screenshot

---
+++ Task.md
@@ -0,0 +1,21 @@
+#### Agila 
+1. Add Logger to every class
+2. Do some error handling
+#### Parul 
+1. Add couple page object
+   MainNav 
+   ContactSale
+   
+### Sowntharya
+Page object - JoinAMeeting
+SignUp 
+
+### Laxmi 
+PAGE - Footer 
+
+### Suresh
+Add capablity for 
+IE, Edge, OPERA, and SAFARI
+
+you need to modify the DriverManager.
+
---
+++ src/test/java/io/pragra/learning/framework/testcases/DemoTest.java
@@ -0,0 +1,42 @@
+package io.pragra.learning.framework.testcases;
+
+import io.pragra.learning.framework.config.Config;
+import io.pragra.learning.framework.drivermanager.DriverManager;
+import io.pragra.learning.framework.pages.RequestADemoPage;
+import io.pragra.learning.framework.pages.TopNav;
+import org.apache.poi.ss.formula.functions.T;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class DemoTest {
+    WebDriver driver ;
+    TopNav topNav;;
+
+    @BeforeSuite
+    public void setUp() {
+        driver = DriverManager.getDriver();
+        driver.get(((String) Config.getProperty("app.url")));
+    }
+
+    @Test
+    public void demoTest() {
+        topNav = new TopNav(driver);
+        RequestADemoPage requestADemoPage = topNav.clickRequest();
+
+        Assert.assertEquals(requestADemoPage.getPageHeading().getText(),"Request a Demo");
+
+        requestADemoPage
+                .keyInEmail("abc@gmail.com")
+                .keyInCompany("ABC CORP")
+                .keyLastName("SS")
+                .keyFirstName("AASDR");
+    }
+
+    @AfterSuite
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(10000);
+        driver.quit();
+    }
+}
+
