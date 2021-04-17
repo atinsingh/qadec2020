7bfb9ff307e5a02fdb9a5b314d699b9de50fb445
Agila Ethiraj <agila.sri21@gmail.com>
2021-04-16 21:45:34 -0400

    Removed hardcoding values

--- src/test/java/io/pragra/learning/framework/reports/HTMLReport.java
+++ src/test/java/io/pragra/learning/framework/reports/HTMLReport.java
@@ -4,6 +4,7 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
+import io.pragra.learning.framework.config.Config;
 import io.pragra.learning.framework.utlis.Utils;
 
 public class HTMLReport {
@@ -15,14 +16,14 @@ public class HTMLReport {
         String file = Utils.getReportFileName();
         extentReports  = new ExtentReports();
         ExtentHtmlReporter reporter = new ExtentHtmlReporter(file);
-        reporter.config().setDocumentTitle("Automation Report");
-        reporter.config().setReportName("Functional Report");
+        reporter.config().setDocumentTitle(Config.getProperty("document.title"));
+        reporter.config().setReportName(Config.getProperty("report.name"));
         reporter.config().setTheme(Theme.DARK);
         extentReports.attachReporter(reporter);
-        extentReports.setSystemInfo("HostName",System.getenv("HOSTNAME"));
-        extentReports.setSystemInfo("OS","MacOS");
-        extentReports.setSystemInfo("Tester","Agila EThiraj");
-        extentReports.setSystemInfo("Browser","Chrome");
+        extentReports.setSystemInfo("HostName",Config.getProperty("HostName"));
+        extentReports.setSystemInfo("OS",Config.getProperty("OS"));
+        extentReports.setSystemInfo("Tester",Config.getProperty("tester"));
+        extentReports.setSystemInfo("browser", Config.getProperty("browser"));
     }
 
     public synchronized static ExtentTest createAndGetTest(String testName) {
